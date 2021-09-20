74fb4b9ee93c690fd0d48132d706417c5d0add8f
alal4465 <alonbenti@gmail.com>
2021-09-21 00:09:15 +0300

    Added ApcDumper implementation

--- ApcDumper/ApcDumper.c
+++ ApcDumper/ApcDumper.c
@@ -1,7 +1,62 @@
 #include <ntddk.h>
+#include <intrin.h>
+#include "ApcSignatures.h"
 
 void DriverUnload(_In_ PDRIVER_OBJECT DriverObject);
 NTSTATUS DriverCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
+
+inline void ResolveKernelRoutines() {
+	UNICODE_STRING InitializeApcRoutineName = RTL_CONSTANT_STRING(L"KeInitializeApc");
+	KeInitializeApc = (KeInitializeApcFunc) MmGetSystemRoutineAddress(&InitializeApcRoutineName);
+
+	UNICODE_STRING InsertApcRoutineName = RTL_CONSTANT_STRING(L"KeInsertQueueApc");
+	KeInsertQueueApc = (KeInsertQueueApcFunc) MmGetSystemRoutineAddress(&InsertApcRoutineName);
+
+	if (!KeInitializeApc || !KeInsertQueueApc) {
+		DbgBreakPoint();
+	}
+
+	KdPrint(("[+] KeInsertQueueApc at: %p\n", KeInsertQueueApc));
+	KdPrint(("[+] KeInitializeApc at: %p\n", KeInitializeApc));
+}
+
+void TestApc(
+	PVOID SystemArgument1,
+	PVOID SystemArgument2,
+	PVOID SystemArgument3,
+	PCONTEXT ContextRecord
+) {
+	KdPrint(("[+] TestApc called\n"));
+	UNREFERENCED_PARAMETER(SystemArgument1);
+	UNREFERENCED_PARAMETER(SystemArgument2);
+	UNREFERENCED_PARAMETER(SystemArgument3);
+	UNREFERENCED_PARAMETER(ContextRecord);
+}
+
+inline void DumpCurrentThreadApcs() {
+	KThread* CurrentThread = (KThread*) KeGetCurrentThread();
+	PKAPC_STATE ApcState = &CurrentThread->ApcState;
+
+	KeAcquireSpinLockAtDpcLevel(&CurrentThread->ThreadLock);
+
+	// dump kernelmode apc's in the current thread
+	for (LIST_ENTRY* CurrentApcListEntry = ApcState->ApcListHead[KernelMode].Flink; CurrentApcListEntry != &ApcState->ApcListHead[KernelMode] ; CurrentApcListEntry = CurrentApcListEntry->Flink) {
+		KAPC* CurrentApc = (KAPC*)((unsigned __int8*)CurrentApcListEntry - offsetof(KAPC, ApcListEntry));
+		
+		// KAPC->Reserved[2] == ApcRoutine
+		KdPrint(("[+] KernelMode Apc: %p\n", CurrentApc->Reserved[2]));
+	}
+
+	// dump usermode apc's in the current thread
+	for (LIST_ENTRY* CurrentApcListEntry = ApcState->ApcListHead[UserMode].Flink; CurrentApcListEntry != &ApcState->ApcListHead[UserMode]; CurrentApcListEntry = CurrentApcListEntry->Flink) {
+		KAPC* CurrentApc = (KAPC*)((unsigned __int8*)CurrentApcListEntry - offsetof(KAPC, ApcListEntry));
+		
+		// KAPC->Reserved[2] == ApcRoutine
+		KdPrint(("[+] UserMode Apc: %p\n", CurrentApc->Reserved[2]));
+	}
+
+	KeReleaseSpinLockFromDpcLevel(&CurrentThread->ThreadLock);
+}
 
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
 	UNREFERENCED_PARAMETER(RegistryPath);
@@ -10,6 +65,21 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
 	DriverObject->DriverUnload = DriverUnload;
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = DriverCreateClose;
 	DriverObject->MajorFunction[IRP_MJ_CLOSE] = DriverCreateClose;
+	
+	ResolveKernelRoutines();
+	PKAPC Apc = ExAllocatePool(NonPagedPool, sizeof(KAPC));
+
+	KIRQL OldIrql;
+	KeRaiseIrql(DISPATCH_LEVEL, &OldIrql);
+
+	// queue a test apc so we'll see something
+	KeInitializeApc(Apc, KeGetCurrentThread(), OriginalApcEnvironment, (void*)ExFreePool, (void*)ExFreePool, (void*)TestApc, KernelMode, NULL);
+	KeInsertQueueApc(Apc, NULL, NULL, 0);
+
+	DumpCurrentThreadApcs();
+
+	KeLowerIrql(OldIrql);
+
 	return STATUS_SUCCESS;
 }
 
